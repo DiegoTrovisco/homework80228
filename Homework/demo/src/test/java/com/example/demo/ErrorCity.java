1f1cb697e44870a619fec061fab79df451b3804d
DiegoTrovisco <34539188+DiegoTrovisco@users.noreply.github.com>
2019-05-06 22:25:07 +0100

    Update ErrorCity.java

--- Homework/demo/src/test/java/com/example/demo/ErrorCity.java
+++ Homework/demo/src/test/java/com/example/demo/ErrorCity.java
@@ -10,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -27,9 +28,15 @@ public class ErrorCity {
 	private MockMvc mvc;
 
 	@Test
-	public void testInternalServerError() throws Exception {
-		//mvc.perform(get("/"))
-		//		.andExpect(status().isInternalServerError());
-            mvc.perform(get("/")).param(nome do @RequestParam, "FailTest")).andExpect("Cidade incorreta")
+	public void testInternalServerErrorCurrent() throws Exception {
+		
+            this.mvc.perform(get("/current").param("city", "Espinho")).andExpect(status().isOk());
+        }
+
+        @Test
+	public void testInternalServerErrorForecast() throws Exception {
+		
+            this.mvc.perform(get("/forecast").param("city", "Espinho")).andExpect(status().isOk());
+
 	}
 }
